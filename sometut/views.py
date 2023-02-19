6c1dd2ddacde136b79dbc64b768dd4bd1af10756
Krunal <@kp1584085@gmail.com>
2023-02-19 19:42:35 +0530

    second

---
+++ sometut/views.py
@@ -0,0 +1,29 @@
+from django import render 
+
+
+def insert(request):
+    if request.method == "POST":
+        saverecord= Personal() 
+        saverecord.username = request.POST.get('username')       
+
+
+# def Insert(request):
+#     if request.method=="POST":
+#         if request.POST.get('Institute_id') and request.POST.get('Institute_name') and request.POST.get('Country'): 
+#             saverecord=Institute()
+#             saverecord.Institute_id=request.POST.get('Institute_id') 
+#             saverecord.Institute_namename=request.POST.get('Institute_name')
+#             saverecord.Country=request.POST.get('Country')
+
+#             allval=Institute.objects.all()
+            
+#             for i in allval:
+#                 if int(i.Institute_id)==int(request.POST.get('Institute_id')):
+#                     messages.warning(request,'Institute already exists....!');
+#                     return render(request,'Insert.html')
+
+#             saverecord.save()
+#             messages.success(request,'Institute '+saverecord.Institute_name+' is saved succesfully!!')
+#             return render(request,'Insert.html')
+#     else:
+#             return render(request,'I
